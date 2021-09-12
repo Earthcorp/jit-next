9d59828dabf3065b0c4139ddd2fe68b755649bca
earthcorp <narendra.s@earthcorp.in>
2021-09-12 09:46:48 +0530

    Update next project

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,13 @@
 export default function Header() {
+
     return (
         <div className="overflow-hidden bg-gray-900">
             <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
                 <div className="flex flex-col items-center justify-between xl:flex-row">
                 <div className="w-full max-w-xl mb-12 xl:pr-16 xl:mb-0 xl:w-7/12">
-                    <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
-                    We provide you the<br className="hidden md:block" />
-                    fastest{' '}
+                    <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-10">
+                    We provide the fastest<br className="hidden md:block" />
+                    {' '}
                     <span className="text-teal-accent-400">App Hosting</span>
                     </h2>
                     <p className="max-w-xl mb-4 text-base text-gray-400 md:text-lg">
@@ -82,7 +83,7 @@ export default function Header() {
                             <input
                             placeholder="john.doe@example.org"
                             required
-                            type="text"
+                            type="email"
                             className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
                             id="email"
                             name="email"
